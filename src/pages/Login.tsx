c6736333771b19ac0ceffcaff5d8a6f35acd9628
Nef3libataa <samanehcosmonaut@gmail.com>
2023-11-20 13:51:34 +0330

    [feat] add formik validation and loginUser function

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useFormik } from 'formik';
-import { theme } from '/src/theme'
-import { PasswordField } from '../components/PasswordField';
+import { theme } from '/src/core/materialconfig/theme.tsx'
+import { PasswordField } from '../core/SharedComponents/PasswordField';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup'
 
-import { Box, Grid, ThemeProvider, Typography, TextField } from '@mui/material';
-import FormButtons from '../components/FormButtons';
+import { Box, Grid, ThemeProvider, Typography, TextField, Button, CssBaseline } from '@mui/material';
+import { loginUser } from '../core/api/Authentication';
 
 
 
@@ -18,44 +20,64 @@ export default function LoginForm() {
         event.preventDefault();
     };
 
+    const navigate = useNavigate();
 
     const formik = useFormik({
         initialValues: {
-            phoneNumber: '',
+            email: '',
             password: '',
         },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email('Invalid email address')
+                .required("Required")
+        }),
         onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+            loginUser(values)
         },
     })
 
     return (
-        <Box
-            component="form"
-            noValidate
-            autoComplete="off"
-            onSubmit={formik.handleSubmit}
-            sx={{ display: 'flex', flexDirection: 'column', height: '100vh', justifyContent: 'space-between' }}
-        >
-            <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                onSubmit={formik.handleSubmit}
+                sx={{ display: 'flex', flexDirection: 'column', height: '100vh', justifyContent: 'space-between' }}
+            >
+                <CssBaseline />
                 <Grid container direction="column" alignItems="center" >
                     <Typography>login</Typography>
 
                     <TextField
-                        id="outlined-number"
-                        label="Phone Number"
-                        type="tel"
+                        id="email"
+                        label="Email"
+                        type='email'
+                        name='email'
                         onChange={formik.handleChange}
-                        value={formik.values.phoneNumber}
+                        value={formik.values.email}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
 
-                    <PasswordField showPassword={showPassword} handleClickShowPassword={handleClickShowPassword} handleMouseDownPassword={handleMouseDownPassword} />
-
+                    <PasswordField
+                        showPassword={showPassword}
+                        handleClickShowPassword={handleClickShowPassword}
+                        handleMouseDownPassword={handleMouseDownPassword}
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
 
                 </Grid>
-                <FormButtons />
-            </ThemeProvider>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
 
-        </Box >
+                    <Button variant="outlined" type="submit" color='secondary' sx={{ mb: 2 }}>login</Button>
+                    <Button variant="contained" type="submit" color='primary' sx={{ mb: 2 }} onClick={() => navigate('/')}>sign up</Button>
+
+                </Box>
+            </Box >
+        </ThemeProvider>
+
     );
 }
