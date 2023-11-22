a5e5ec925e396a6c3565525c71c0370be0ad2b8b
Nef3libataa <samanehcosmonaut@gmail.com>
2023-11-22 11:15:53 +0330

    [feat] add snackbar and edit routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import FormPropsTextFields from './pages/SignUp'
 import Login from './pages/Login'
+import Profile from './pages/Profile'
+import SnackbarComponent from './core/SharedComponents/Snackbar'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from './core/models/redux.model'
+import { closeSnackbar } from './state/actionCreators'
 
 function App() {
 
+  const snackbar = useSelector((state: RootState) => state.snackbar)
+  const dispatch = useDispatch();
+
+  const handleCloseSnackbar = () => {
+    dispatch(closeSnackbar())
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<FormPropsTextFields />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+
+      <SnackbarComponent isOpen={snackbar.isOpen} message={snackbar.message} handleClose={handleCloseSnackbar} severity={snackbar.severity}/>
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<FormPropsTextFields />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
