b327d8afeccd7ca92063a744b4d556cca898896a
Nef3libataa <samanehcosmonaut@gmail.com>
2023-11-22 11:17:51 +0330

    [feat] add loginUser function

--- src/core/api/Authentication.ts
+++ src/core/api/Authentication.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
+import { UserValuesPayload } from "../models/api/register.model";
+import { LoginValuesPayload } from "../models/api/login.model";
 
-interface UserValues {
-  phoneNumber: string;
-  email: string;
-  password: string;
-}
-
-export const registerUser = async (values: UserValues) => {
+export const registerUser = async (values: UserValuesPayload) => {
   axios
     .post("http://localhost:3000/register", {
       phoneNumber: values.phoneNumber,
@@ -19,4 +15,12 @@ export const registerUser = async (values: UserValues) => {
     .catch((error) => {
       console.log(error);
     });
+};
+
+export const loginUser = async (values: LoginValuesPayload) => {
+  return axios
+    .post("http://localhost:3000/login", {
+      email: values.email,
+      password: values.password,
+    })
 };
