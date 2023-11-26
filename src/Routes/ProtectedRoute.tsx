86d3cc964888f44f781d49bb6ee8a6769c04ee81
Nef3libataa <samanehcosmonaut@gmail.com>
2023-11-26 13:57:38 +0330

    [chore] remove space

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom';
 
-
 export const ProtectedRoute = ({ children }) => {
   const accessToken = localStorage.getItem('accessToken');
   if (!accessToken) {
